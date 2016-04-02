79b52e785f5b191163ac227632b67fe6eb1cf079
Lizzy95 <lizzi16.gd95@gmail.com>
2016-04-02 14:33:56 -0600

    aaa

---
+++ QuimiNet/QuimiNet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuimiNet
+//
+//  Created by Lizzy Cruz on 01/04/16.
+//  Copyright © 2016 Lizzy Cruz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
